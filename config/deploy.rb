9c53870a5d54fafeaf13524c617f119584ed6c19
xorphitus <xorphitus@gmail.com>
2014-06-22 00:19:11 +0900

    passengerのrestart対応

--- config/deploy.rb
+++ config/deploy.rb
@@ -41,7 +41,8 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :mkdir, release_path.join('tmp') unless File.exist? release_path.join('tmp')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
